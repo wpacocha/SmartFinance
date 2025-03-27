b973e496b1a0aee73363ba9edef13d84b0a5fab6
Oliwia Obiegła <148152881+aaszlii@users.noreply.github.com>
2025-03-27 20:23:35 +0100

    add unit tests

---
+++ SmartFinance.Tests/ExchangeRateControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartFinance.API.Controllers;
+using SmartFinance.API.Data;
+using SmartFinance.API.Models;
+using SmartFinance.API.Services;
+using System;
+using System.Threading.Tasks;
+
+public class FakeExchangeRateService : ExchangeRateService
+{
+    public FakeExchangeRateService() : base(new HttpClient()) { }
+    public override Task<ExchangeRate?> GetRateAsync(string currency)
+    {
+        return Task.FromResult<ExchangeRate?>(new ExchangeRate
+        {
+            Currency = currency,
+            Rate = 4.5m,
+            Date = DateTime.Today
+        });
+    }
+}
+
+public class ExchangeRateControllerTests
+{
+    [Fact]
+    public async Task GetRate_ReturnsExchangeRate()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<FinanceDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        var context = new FinanceDbContext(options);
+        var fakeService = new FakeExchangeRateService(); 
+
+        var controller = new ExchangeRateController(fakeService, context);
+
+        // Act
+        var result = await controller.GetRate("USD");
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returned = Assert.IsType<ExchangeRate>(okResult.Value);
+        Assert.Equal("USD", returned.Currency);
+    }
+}
---
+++ SmartFinance.Tests/ReportControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using SmartFinance.API.Controllers;
+using SmartFinance.API.Data;
+using SmartFinance.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+
+public class ReportControllerTests
+{
+    [Fact]
+    public void GetMonthlySummary_ReturnsCorrectValues()
+    {
+        var options = new DbContextOptionsBuilder<FinanceDbContext>()
+            .UseInMemoryDatabase("GetMonthlySummary")
+            .Options;
+
+        using (var context = new FinanceDbContext(options))
+        {
+            context.Transactions.AddRange(new List<Transaction>
+            {
+                new Transaction { Amount = 1000, IsIncome = true, UserId = 1, Month = 3, Year = 2025 },
+                new Transaction { Amount = 300, IsIncome = false, UserId = 1, Month = 3, Year = 2025 }
+            });
+            context.SaveChanges();
+
+            var controller = new ReportController(context);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new[] {
+                        new Claim(ClaimTypes.NameIdentifier, "1")
+                    }, "mock"))
+                }
+            };
+
+            var result = controller.GetMonthlySummary(3, 2025);
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
